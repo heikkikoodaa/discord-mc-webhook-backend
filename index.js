360a4873ceb1782e41b42439a7a4cc64d2c6540f
torastrengel <hekuttaja@gmail.com>
2022-07-26 15:58:52 +0300

    Initial commit 🎉😸

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const axios = require('axios').default;
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.post('/message', (req, res) => {
+  const { message } = req.body;
+
+  axios
+    .post(WEBHOOK_URL, {
+      content: `<@&651412869606277140> ${message}`,
+    })
+    .then((response) => {
+      console.log(response);
+      res
+        .status(200)
+        .json({ status: 200, message: 'Message successfully sent!' });
+    })
+    .catch((error) => {
+      res.status(400).json({ status: 400, message: error });
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
